d34682e6c37926ae8b45f1491e8904e1de278491
rburunkayaSocure <100721379+rburunkayaSocure@users.noreply.github.com>
2024-04-05 15:56:35 -0400

    Initial Commit

---
+++ SFSafariViewContoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFSafariViewContoller
+//
+//  Created by Recep Burunkaya on 4/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
